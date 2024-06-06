6d9038479e76e69b6a10cd3c3580bd5088b2359c
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-06 22:19:07 +0200

    downloaded dependencies. mapped pages

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { AppBar } from '@mui/material'
 
 export default function Header() {
+
+  const pages: string[] = ["Home", "Music", "Coding", "Languages", "Fitness"]
   return (
-    <div>Header</div>
+  <div>
+    {pages.map((page)=> <h1> {page}</h1>)}
+  </div>
   )
 }
