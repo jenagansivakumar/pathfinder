d195cb8e7f3f6d2c1a3f77fca5bad1ce5a63ba96
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-06 23:04:18 +0200

    added buttons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { AppBar } from '@mui/material'
+import React from 'react';
+import { AppBar, Toolbar, Box, Button } from '@mui/material';
 
 export default function Header() {
+  const pages: string[] = ["Home", "Music", "Coding", "Languages", "Fitness"];
 
-  const pages: string[] = ["Home", "Music", "Coding", "Languages", "Fitness"]
   return (
-  <div>
-    {pages.map((page)=> <h1> {page}</h1>)}
-  </div>
-  )
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'space-between' }}>
+            {pages.map((page) => (
+              <Button  variant='contained' key={page} sx={{ color: 'white', backgroundColor: 'blue' }} disableRipple>
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
