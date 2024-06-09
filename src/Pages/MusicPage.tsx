24832512bc71aa849a10ade8ceb0bf6ecbda2d9d
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-09 20:07:44 +0200

    creating form to add images

--- src/Pages/MusicPage.tsx
+++ src/Pages/MusicPage.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import { useState } from 'react'
+import { FormControl, FormLabel, Button } from '@mui/material'
 
 export default function MusicPage() {
 
-  const [image,setImage] = useState("")
-  function handleImage(){
+  const [image,setImage] = useState<File | null>(null)
+  function handleFile(e: React.ChangeEvent<HTMLInputElement>){
+    if (e.target.files && e.target.files.length > 0){
+    setImage(e.target.files[0])}
 }
 
   return (
     <>
      <Navbar />
         <div>MusicPage</div>
-        <input type='file' name='file' onChange={handleFile}/>
-        <button onSubmit={ }> Submit</button>
+        <FormControl>
+          <FormLabel> Add image</FormLabel>
+          <input type='file' name='file' onChange={handleFile}/>
+          <Button > Submit</Button>
+        </FormControl>
     </>
   )
 }
