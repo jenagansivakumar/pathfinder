46c3f24a93162517c5e7ef5536eb5948ae9f188f
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-09 16:05:02 +0200

    creating image upload functionality

--- src/Pages/MusicPage.tsx
+++ src/Pages/MusicPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { useState } from 'react'
 
 export default function MusicPage() {
+
+  const [image,setImage] = useState("")
+  function handleImage(){
+}
+
   return (
     <>
-    <Navbar />
-    <div>MusicPage</div>
+     <Navbar />
+        <div>MusicPage</div>
+        <input type='file' name='file' onChange={handleFile}/>
+        <button onSubmit={ }> Submit</button>
     </>
   )
 }
