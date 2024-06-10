2518006e85d6facc939e5d8e972f6ed25eb76836
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-10 21:31:24 +0200

    creating weather app component

---
+++ src/Components/WeatherApp/WeatherApp.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Paper, TextField, Button } from '@mui/material'
+
+export const WeatherApp = () => {
+  return (
+    <>
+        <Paper>
+            <form>
+                <TextField />
+                <Button> Submit </Button>
+            </form>
+        </Paper>
+    </>
+  )
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import ToDoForm from '../Components/ToDoList/ToDoForm'
+import { Typography, colors } from '@mui/material'
+import { WeatherApp } from '../Components/WeatherApp/WeatherApp'
 
 
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <h1> KRISTINA </h1>
-    <ToDoForm />
+    <Typography variant='h3'> WELCOME TO</Typography>
+    <Typography variant='h1'> PATHFINDER</Typography>
+    <WeatherApp/>
     </>
   )
 }
