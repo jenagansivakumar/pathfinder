b69a050870600056969794caceb9b6e233509e83
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-10 19:05:36 +0200

    createing a remove button to delete list items

--- src/Components/ToDoList/ToDoForm.tsx
+++ src/Components/ToDoList/ToDoForm.tsx
@@ -1,28 +1,49 @@
-import { TextField, Container, Button } from '@mui/material'
-import { log } from 'console'
-import React from 'react'
-import { useState } from 'react'
+import { TextField, Container, Button, Typography, List, ListItem } from '@mui/material';
+import React, { useState } from 'react';
 
 export default function ToDoForm() {
-    const [todos, setTodos] = useState<string[]>([])
-    const [input, setInput] = useState<string>("")
-    function onChange(e: React.ChangeEvent<HTMLInputElement>){
-        console.log(e)
-        setInput(e.target.value)
+    const [todos, setTodos] = useState<string[]>([]);
+    const [input, setInput] = useState<string>("");
+
+    function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+        // console.log(e);
+        setInput(e.target.value);
     }
 
-    function handleAddItem(e: React.MouseEvent<HTMLElement>){
-      if (input.trim() !== ""){
-        setTodos([...todos, input])
-        setInput("")
-      }
-
+    function handleAddItem(e: React.MouseEvent<HTMLElement>) {
+        if (input.trim() !== "") {
+            setTodos([...todos, input]);
+            setInput("");
+        }
     }
 
-  return (
-    <Container className='ToDoForm'>
-        <TextField variant='outlined' value={input} onChange={onChange}  ></TextField>
-        <Button onClick={handleAddItem}> Add Todo</Button>
-    </Container>
-  )
+    function handleRemove(id:any){
+      console.log(id)
+    }
+
+    return (
+        <Container className='ToDoForm'>
+            <TextField
+                variant='outlined'
+                value={input}
+                onChange={onChange}
+                placeholder="Add new task"
+            />
+            <Button onClick={handleAddItem} variant="contained" color="primary">
+                Add Todo
+            </Button>
+            <Typography variant="h6" component="div" gutterBottom>
+                To-Do List
+            </Typography>
+            <List>
+                {todos.map((todo, index) => (
+                    <ListItem key={index}>{todo}
+                    <Button onClick={handleRemove}> Remove </Button>
+                    </ListItem>
+                    
+                    
+                ))}
+            </List>
+        </Container>
+    );
 }
