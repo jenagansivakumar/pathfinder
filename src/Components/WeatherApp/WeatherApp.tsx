3ac9308894557e3c537a1d867eff07324566e006
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-06-12 16:31:42 +0200

    created interface for WeatherData

--- src/Components/WeatherApp/WeatherApp.tsx
+++ src/Components/WeatherApp/WeatherApp.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
-import { Paper, TextField, Button, Typography } from '@mui/material'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { Paper, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
 
-export const WeatherApp = () => {
 
+
+interface WeatherData {
+    list: {
+        main: {
+            temp: number
+        }
+        weather: {
+            icon: string
+        }[]
+    }[]
+}
+export const WeatherApp: React.FC = () => {
+    const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+    const fetchWeatherData = (cityName: string) => {
+        const apiKey = process.env.REACT_APP_API_KEY
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Error fetching weather data")
+            }
+            return response.json()
+        })
+        .then((data: WeatherData) => setWeatherData(data))
+
+    }
+  
+  const handleSubmit = (event: React.MouseEvent) =>{
+    event.preventDefault()
+    fetchWeatherData()
+  }
 
   return (
     <>
-        <Paper>
-            <form>
-            <Typography variant='h6'> City Name </Typography>
-                <TextField fullWidth />
-                <Button> Submit </Button>
-            </form>
-            <img src='' alt='weather icon' />
-        </Paper>
+      <Paper>
+        <form>
+          <Typography variant='h6'>City Name</Typography>
+          <TextField fullWidth />
+          <Button>Submit</Button>
+        </form>
+        <img src='' alt='weather icon' />
+      </Paper>
     </>
-  )
-}
+  );
+};
